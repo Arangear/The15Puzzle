dc24ad4df3e44c06a40f60d774418549041b05c9
Daniel Cieslowski <cieslowskids@gmail.com>
2019-10-16 14:51:38 +0100

    Added abstract FileManager class

---
+++ The15Puzzle/Coursework1/FileManager.h
@@ -0,0 +1,13 @@
+//Author:        Daniel Cieslowski
+//Date created:  16.10.2019
+//Last modified: 16.10.2019
+#pragma once
+
+#include <fstream>
+
+class FileManager
+{
+public:
+	virtual void OpenStream(std::string filePath) = 0;
+	virtual void CloseStream() = 0;
+};
