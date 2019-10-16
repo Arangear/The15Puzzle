da9067ab263d20811a5d4d9669b4962ea43cb693
Daniel Cieslowski <cieslowskids@gmail.com>
2019-10-16 14:52:41 +0100

    Cleaned up main function

--- The15Puzzle/Coursework1/Coursework1.cpp
+++ The15Puzzle/Coursework1/Coursework1.cpp
@@ -6,17 +6,5 @@
 
 int main()
 {
-	Puzzle p1;
-	Puzzle p2;
-	for (int i = 0; i < p2.GetSize(); i++)
-	{
-		for (int j = 0; j < p2.GetSize(); j++)
-		{
-			p2(i, j) = i * (i + 1) + j - 3;
-		}
-	}
-	Puzzle p3(p2);
-	std::cout << p1 << "\n" << p2 << "\n" << p3;
-
 	return 0;
 }
