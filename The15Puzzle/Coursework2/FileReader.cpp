49048dd5024a8b09afe2a239a87319a90b04df88
Daniel Ciesłowski <cieslowskids@gmail.com>
2019-10-24 20:25:24 +0100

    Added file input validation

--- The15Puzzle/Coursework2/FileReader.cpp
+++ The15Puzzle/Coursework2/FileReader.cpp
@@ -4,6 +4,7 @@
 #include "FileReader.h"
 #include <string>
 #include <limits>
+#include <set>
 
 bool FileReader::openStream(const std::string filePath)
 {
@@ -19,12 +20,20 @@ void FileReader::closeStream()
 result FileReader::loadPuzzles(const int count, const int size, std::deque<Puzzle>& puzzles)
 {
 	Puzzle puzzle(size);
+	std::set<int> values;
 	for (int i = 0; i < count; i++)
 	{
+		values.clear();
 		for (int j = 0; j < puzzle.ElementCount(); j++)
 		{
 			stream >> puzzle(j);
 			if (stream.fail())
+			{
+				stream.clear();
+				return readFail;
+			}
+			values.insert(puzzle(j));
+			if (values.size() != count + 1)
 			{
 				stream.clear();
 				return readFail;
